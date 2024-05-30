ac54fd98bd3f226e7f3b2b417a53933bd0499b4a
RJ <rayjohndelapena07@gmail.com>
2024-05-30 11:13:12 +0800

    Update Tempalte for Flask App

--- template.py
+++ template.py
@@ -14,13 +14,18 @@ list_of_files = [
     f"src/{project_name}/config/configuration.py",
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py"
     f"src/{project_name}/constants/__init__.py",
     "config/config.yaml",
-    "dvc.yaml",
     "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "Dockerfile",
     "requirements.txt",
     "setup.py",
-    "research/trials.ipynb"
+    "research/trials.ipynb",
+    "templates/index.html"
 ]
 
 for filepath in list_of_files:
